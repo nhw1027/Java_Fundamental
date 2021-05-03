dcaf73474a25e296dd307a8bfb310d617d9be366
nhw1027 <nhwee1027@gmail.com>
2021-05-03 17:29:21 +0900

    2021-05-03

---
+++ src/java_20210503/ContinueDemo.java
@@ -0,0 +1,12 @@
+package java_20210503;
+
+public class ContinueDemo {
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			if(i == 6) continue;
+			sum += i;
+		}
+		System.out.println("합은 : " + sum);
+	}
+}
